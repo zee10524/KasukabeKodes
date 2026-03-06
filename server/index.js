913053af3f505207cef4b87eb8ec89585701fba7
Mohammad Zeeshan Ahmad <zeeshan10524@gmail.com>
2026-03-06 09:49:24 +0530

    initial commit

--- server/index.js
+++ server/index.js
@@ -1,27 +1,34 @@
 const express = require("express");
-const passport = require("passport");
 const cors = require("cors");
+const passport = require("passport");
 require("dotenv").config();
+
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
+// Connect to database
 connectDB();
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(passport.initialize());
 
+// Passport configuration
 require("./passport")(passport);
 
-app.use("/api/auth", require("./routes/auth"));
+// Routes
+app.use("/api/auth", authRoutes);
 
+// Test route
 app.get("/", (req, res) => {
-  res.send("Server Running");
+  res.send("Server is running");
 });
 
+// Start server
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server running on port", PORT);
 });
